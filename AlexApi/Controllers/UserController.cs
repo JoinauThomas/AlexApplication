0af0099b0ab9d81cdbad782ab833c272f6809a0e
Thomas Joinau <Thomas.Joinau@ingenico.com>
2020-05-28 01:32:12 +0200

    8 - inscription + login coté serveur

--- AlexApi/Controllers/UserController.cs
+++ AlexApi/Controllers/UserController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlexApi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlexApi.Controllers
@@ -13,12 +15,74 @@ namespace AlexApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<MyUser> userManager;
+        private readonly SignInManager<MyUser> signInManager;
+
+        public UserController(UserManager<MyUser> userManager, SignInManager<MyUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         [Route("GetClaims")]
         [Authorize]
         public IActionResult GetClaims()
         {
             return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+        [HttpPost]
+        [Route("CreateNewUser")]
+        public async Task<IActionResult> CreateNewUserAsync(UserSubscriptionModel newUser)
+        {
+            IdentityResult result = new IdentityResult();
+            result = await userManager.CreateAsync(new MyUser
+            {
+                UserName = newUser.Mail,
+                Email = newUser.Mail,
+                Nom = newUser.Nom,
+                Prenom = newUser.Prenom,
+                Mobile = newUser.Mobile,
+                Sexe = newUser.Sexe,
+                DateDeNaissance = newUser.DateDeNaissance,
+                Professionnel = newUser.Professionnel
+
+            }, newUser.Password);
+
+
+            if (result.Succeeded)
+            {
+                return Created("mysite.com", result);
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.Code, item.Description);
+                }
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginModel login)
+        {
+            var result = await signInManager.PasswordSignInAsync(
+                userName: login.Email,
+                password: login.Password,
+                isPersistent: true,
+                lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                ModelState.AddModelError("Login impossible", "Unne erreur s'est produite lors de la tentative de connexion");
+                return BadRequest(ModelState);
+            }
         }
     }
 }
---
+++ AlexApi/Models/UserSubscriptionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlexApi.Models
+{
+    public class UserSubscriptionModel
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Mobile { get; set; }
+        public string Sexe { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+        public bool Professionnel { get; set; }
+    }
+}
